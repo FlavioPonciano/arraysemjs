2912292c05eff8e9f75aa13fb94f0d2f5c47dc55
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-07-20 20:03:54 -0300

    Iniciando os estudos de Arrays

---
+++ array-basico/index.js
@@ -0,0 +1,24 @@
+/* 
+
+Arrays
+São listas ordenadas
+Itens que são relacionados à indices ou chamados chaves
+O Array permite que seja armazenada toda uma lista de itens em uma variável, fazendo uma associação com indices
+
+*/
+
+// Variáveis:
+var variavel1 = 'Banana'
+var variavel2 = 'Maçã'
+var variavel3 = 'Morango'
+var variavel4 = 'Uva'
+
+// Com um único Array:
+var listaFrutas = Array()
+listaFrutas['0'] = 'Melancia'
+listaFrutas['1'] = 'Banana'
+listaFrutas['2'] = 'Maçã'
+listaFrutas['3'] = 'Morango'
+listaFrutas['4'] = 'Uva'
+
+console.log(listaFrutas)
