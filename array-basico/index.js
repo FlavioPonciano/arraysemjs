4dc98ae11085053c931a45b0db06e8bffb2f49f0
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-07-20 20:15:50 -0300

    Update index.js

--- array-basico/index.js
+++ array-basico/index.js
@@ -14,11 +14,16 @@ var variavel3 = 'Morango'
 var variavel4 = 'Uva'
 
 // Com um único Array:
-var listaFrutas = Array()
+var listaFrutas = Array() // também podendo ser chamado var listaFrutas = [] podendo ser manipulando normalmente
 listaFrutas['0'] = 'Melancia'
 listaFrutas['1'] = 'Banana'
-listaFrutas['2'] = 'Maçã'
+listaFrutas['2'] = True
 listaFrutas['3'] = 'Morango'
-listaFrutas['4'] = 'Uva'
+listaFrutas['4'] = 100
+//O indice não precisa ser número ou sequencial, ele precisa ser unico e pode ser mesclado
 
-console.log(listaFrutas)
+console.log(listaFrutas[2]) //Pode ser solicitado a impressão de toda a array ou de um indice
+
+//Array pode ser declado assinm tamém:
+var listaFrutas = Array('Mamão', 'laranja')
+var listaFrutas = ['Mamão', 'laranja']
